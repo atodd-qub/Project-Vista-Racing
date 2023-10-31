8622663bf234f8f7c874ad418a67e320a6022395
atodd-qub <atodd11@qub.ac.uk>
2023-10-31 00:52:30 +0000

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private bool isAccelerating;
+    private bool isBreaking;
+    public KeyCode brakeKey;
+    public KeyCode accelerateKey;
+    public KeyCode driftKey;
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+
+    public float maxSteeringAngle = 30f;
+    public float motorForce = 50f;
+    public float brakeForce = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        //UpdateWheels();
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBreaking = Input.GetKey(brakeKey);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        brakeForce = isBreaking ? 3000f : 0f;
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        steerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+        frontRightWheelCollider.steerAngle = steerAngle;
+    }
+}
