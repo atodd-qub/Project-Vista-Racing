33ebd85279a05949fa8b6a1e747b77dd2fd92ede
atodd-qub <atodd11@qub.ac.uk>
2023-12-12 00:35:04 +0000

    Buttons and menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,21 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public Button btnPlay;
     public Button btnHowToPlay;
+    public Button btnHowToPlayClose;
+
+    public GameObject objHowToPlay;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnButtonPress(string buttonValue)
+    {
+        switch (buttonValue)
+        {
+            case "Play":
+                {
+                    // load scene...too scared to rename the scene at this point so its still called samplescene lol
+                    SceneManager.LoadScene("SampleScene");
+                    break;
+                }
+            case "HowToPlay":
+                {
+                    // activate how to play game object
+                    objHowToPlay.SetActive(true);
+                    break;
+                }
+            case "HowToPlayClose":
+                {
+                    // close
+                    objHowToPlay.SetActive(false);
+                    break;
+                }
+            case "Retry":
+                {
+                    // reload current scene
+                    
+                    break;
+                }
+            case "MainMenu":
+                {
+                    // load mainmenu
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+                }
+            default:
+                {
+                    print("Button error");
+                    break;
+                }
+        }
     }
 }
